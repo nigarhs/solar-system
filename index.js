404df44fbc0bc2a46d91978b1a50e785d22ce0f5
nigar <nigarhasanova02@gmail.com>
2024-02-24 21:47:06 +0400

    done

---
+++ index.js
@@ -0,0 +1,55 @@
+const planets = {
+  description: "planets list",
+  planetList: [
+    {
+      name: "Mercury",
+      url: "./images/mercury.png",
+      g: '3.7',
+    },
+    {
+      name: "Venus",
+      url: "./images/venus.png",
+      g : '8.9'
+    },
+    {
+      name: "Earth",
+      url: "./images/earth.png",
+      g : '9.8'
+    },
+    {
+      name: "Moon",
+      url: "./images/moon.png",
+      g : '1.62'
+    },
+    {
+      name: "Mars",
+      url: "./images/mars.png",
+      g : '3.7'
+    },
+    {
+      name: "Jupiter",
+      url: "./images/jupiter.png",
+      g : '24.8'
+    },
+    {
+      name: "Saturn",
+      url: "./images/saturn.png",
+      g : '10.4'
+    },
+    {
+      name: "Uranus",
+      url: "./images/uranus.png",
+      g : '8.9'
+    },
+    {
+      name: "Neptune",
+      url: "./images/neptune.png",
+      g : '11.2'
+    },
+    {
+      name: "Pluto",
+      url: "./images/pluto.png",
+      g : '0.58'
+    },
+  ],
+};
